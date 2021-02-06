c7d2be7f3e19fc36c226d46f4552a0162f71eff5
MaedaToshinori <65947565+MaedaToshinori@users.noreply.github.com>
2021-02-06 15:08:08 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
+#セキュリティの問題乗非表示にしています
 CONSUMER_KEY = "********************" #API key
 CONSUMER_SECRET = "********************" #API secret key
 ACCESS_TOKEN = "********************" #Access token
-ACCESS_TOKEN_SECRET = "" #Access token secret
+ACCESS_TOKEN_SECRET = "********************" #Access token secret
