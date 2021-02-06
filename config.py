5fe2ae65f7264c86d8b779ea0e31c18df53265a3
MaedaToshinori <65947565+MaedaToshinori@users.noreply.github.com>
2021-02-06 15:07:24 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-CONSUMER_KEY = "jPkzfXzsiAKQHVUR5LwaRL6pa" #API key
-CONSUMER_SECRET = "e7RldzjYC6G5aWa66Cat0sxf1cgKgF3dmlOCIjUTvVArbbNVGt" #API secret key
-ACCESS_TOKEN = "2609261629-G4THyx01YUaHVCohHmW0HOjotEOXHbHoZcY6C5X" #Access token
-ACCESS_TOKEN_SECRET = "nOtPNTPhgzoMP8OybnddniSRizjWkwnCvo9wEZ0Vmja4C" #Access token secret
+CONSUMER_KEY = "********************" #API key
+CONSUMER_SECRET = "********************" #API secret key
+ACCESS_TOKEN = "********************" #Access token
+ACCESS_TOKEN_SECRET = "" #Access token secret
